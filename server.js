4e6419f4a30e2587a1147fd1e185a8151f3f3e4a
malex19889 <malex19889@yahoo.com>
2020-11-19 13:37:44 -0600

    changed dev db

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+require("dotenv").config();
 const PORT = process.env.PORT || 8080;
 
 
@@ -14,7 +15,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/Workout-Tracker', {
+  process.env.MONGODB_URI || 'mongodb://localhost/workout', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
